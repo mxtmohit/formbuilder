9f6f37997027e8f65cf86ca6b13788a4ff0755dc
mxtmohit <81439541+mxtmohit@users.noreply.github.com>
2023-10-30 20:14:33 +0530

    ready to host

--- Frontend/src/Pages/Formdesign/Formpage.js
+++ Frontend/src/Pages/Formdesign/Formpage.js
@@ -216,7 +216,7 @@ const Formpage = ({ formid1, qnArraymain1, titleobj1, formDataValues,starttime,e
       if (res.status == 200) {
         setIsOpen(true);
         setFormid(res.data.id);
-        navigator.clipboard.writeText(`http://localhost:3000/form/${formid}`);
+        navigator.clipboard.writeText(`/form/${formid}`);
         setType("success");
         setmessage(
           "Form Successfully Uploaded and Link copied to the ClipBoard"
